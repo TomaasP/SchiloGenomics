d12a593907c11d0691744e9164039ee49e4a80ba
TomaasP <59613937+TomaasP@users.noreply.github.com>
2024-06-25 12:11:32 -0400

    Update PopGenomeAnalysis.R

--- Short Reads/04.PopulationGenomicsStats/PopGenomeAnalysis.R
+++ Short Reads/04.PopulationGenomicsStats/PopGenomeAnalysis.R
@@ -15,8 +15,8 @@ setwd("PathToWorkingDIrectory")
 # In case of crashing, you need to load each chromosome appart and merged them later.
 Whole_genome <- readData("PathToVCF", format = "VCF")
 get.sum.data(Whole_genome) # Data object summary
-# Añadir info de poblaciones.
-#Crear una lista con titulo de columna y codigo de cepas exactamente igual al que estè en el vcf en formato .txt
+# Add population info, accordinlgy to the ADMIXTURE analysis.
+# We need a tabulated file {pop.txt, for example} with the ID and their respective population.
 Hol <- read.table("Hol.strains.txt", sep = "\t", header = T)
 Hol %>% pull(strain) %>% as.character() -> Hol
 
@@ -38,9 +38,9 @@ SA_C %>% pull(strain) %>% as.character() -> SA_C
 Auss <- read.table("Auss.strains.txt", sep = "\t", header = T)
 Auss %>% pull(strain) %>% as.character() -> Auss
 
-SoAm <- c(SA_A,SA_B1,SA_B2,SA_B3,SA_C)
+SoAm <- c(SA_A,SA_B1,SA_B2,SA_B3,SA_C) # Here we can stablished different levels of comparisson. Whole southamerico or sub set of populations
 
-#setear linajes 
+# Lineages setting 
 Whole_genome<-set.populations(Whole_genome,list(Hol, SA_A, SA_B1, SA_B2, SA_B3,SA_C,Auss))
 Whole_genome@populations
 
@@ -48,22 +48,22 @@ Whole_genome<-set.populations(Whole_genome,list(Hol,Auss,SoAm))
 Whole_genome@populations
 
 
-#Comenzar an?lisis
+# Start anlaysis 
 Whole_genome<-concatenate.regions(Whole_genome)
 Whole_genome
 Whole_genome@region.names 
-Whole_genome@n.sites2 #numero de snp 
-  #FST
+Whole_genome@n.sites2 # SNPs numbers
+#FST
 Whole_genome<-F_ST.stats(Whole_genome,mode="nucleotide")
 pairwise.FST <- t(Whole_genome@nuc.F_ST.pairwise)
 head(pairwise.FST)
 
-write.table(pairwise.FST, file = "Fst_3Pop", sep = "\t",
+write.table(pairwise.FST, file = "Example name", sep = "\t",
             row.names = F, col.names = T)
-  #Pi
+#Pi
 Whole_genome <- diversity.stats(Whole_genome)
-nucdiv <- Whole_genome@nuc.diversity.within/12000000
+nucdiv <- Whole_genome@nuc.diversity.within/12000000 # divided by the total genome size to obtain the per site value
 head(nucdiv)
 
-write.table(nucdiv, file = "Pi_3Pop", sep = "\t",
+write.table(nucdiv, file = "Example name", sep = "\t",
             row.names = F, col.names = T)
